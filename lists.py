4d83aae6a14a3d9fbb18c378c9297e3579a48a3c
jnprogrammer <joshuanparker@gmail.com>
2020-05-03 19:15:01 -0400

    first push

---
+++ lists.py
@@ -0,0 +1,10 @@
+list_1 = []
+list_2 = list()
+
+print("List 1: {}".format(list_1))
+print("List 2: {}".format(list_2))
+
+if list_1 == list_2:
+    print("The lists are equal")
+
+print(list("The lists are equal"))
