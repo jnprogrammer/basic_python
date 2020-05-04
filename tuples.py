e903b371dfba13d0dc28251243bf1ee06bbc3e68
joshuaparker <joshuanparker@gmail.com>
2020-05-04 14:17:58 -0400

    Added some work with tuples

---
+++ tuples.py
@@ -0,0 +1,32 @@
+# t = "a", "b", "c"
+# print(t)
+#
+# print("a","b","c")
+# print(("a", "b", "c"))
+# Tuples are immutable
+
+welcome = "Welcome to my Nightmare" , "Alice Cooper", 1975
+bad = "Bad Company" "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+emdela = "More Mayhem", "imilda May", 2011
+metallica = "Ride the lighting", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+# metallica[0] = "Masters of Pupppest"
+emdela = emdela[0], "Emelda May", emdela[2]
+print(emdela)
+
+metallica2 = ["Ride the Lighting", "Metallica", 1984]
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)
+
+
+title, artist, year = emdela
+print(title)
+print(artist)
+print(year)
