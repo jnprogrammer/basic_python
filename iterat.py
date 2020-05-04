0f75998de211e8eb0b2a251e4f1f4f0c644a6412
jnprogrammer <joshuanparker@gmail.com>
2020-05-03 20:33:16 -0400

    iterator script

---
+++ iterat.py
@@ -0,0 +1,16 @@
+string = "1234567890"
+
+#for char in string:
+#    print(char)
+
+my_iterator = iter(string)
+print(my_iterator)
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
