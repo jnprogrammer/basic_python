adf5f95ea8b64967c2d511162c52ecd50137d219
joshuaparker <joshuanparker@gmail.com>
2020-05-05 22:16:51 -0400

    made an upgraded version of the game

---
+++ dictionaries4.py
@@ -0,0 +1,6 @@
+fruit = {"orange" : "A sweet orange, citrus fruit",
+         "apple" : "good for making cider",
+         "lemon" : "a small, sour citrus fruit",
+         "grape" : "A sweet, small bunched fruit",
+         "lime" : "A sour, green citrus fruit"}
+
