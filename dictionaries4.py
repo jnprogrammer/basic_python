3bc86d06f0316a72bad27e68458140fc927b95f2
joshuaparker <joshuanparker@gmail.com>
2020-05-05 22:41:25 -0400

    Used multiple dictionaries to allow different user input for directions in the game.

--- dictionaries4.py
+++ dictionaries4.py
@@ -4,3 +4,25 @@ fruit = {"orange" : "A sweet orange, citrus fruit",
          "grape" : "A sweet, small bunched fruit",
          "lime" : "A sour, green citrus fruit"}
 
+print(fruit)
+
+veg = {"Cabbage" : "Great with corn beef",
+       "Sprouts": "mmmmm, lovely",
+       "spinach" : "So yummy on anything!!"}
+
+print(veg)
+
+veg.update(fruit)
+print(veg)
+print( " %% " * 50)
+fruit.update(veg)
+
+print(fruit.update(veg))
+print(fruit)
+
+nice_and_nasty = fruit.copy()
+nice_and_nasty.update(veg)
+
+print(nice_and_nasty)
+print(veg)
+print(fruit)
