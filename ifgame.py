819bb77644c1d57c850a8de4a4b24285e38f27e4
joshuaparker <joshuanparker@gmail.com>
2020-05-07 23:43:08 -0400

    added 2nd try to if game

--- ifgame.py
+++ ifgame.py
@@ -1,11 +1,22 @@
 answer = 7
 
-guess = int(input("Please guess number between 1 and 1o: "))
+guess = int(input("Please guess number between 1 and 10: "))
+
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("You did it")
+    else:
+        print("You didn't get it in two tries")
 elif guess > answer:
     print("Please guess lower")
-else:
+    guess = int(input())
+    if guess == answer:
+        print("You did it")
+    else:
+        print("You didn't get it in two tries")
+elif guess > answer:
     print("you got it first time")
 
