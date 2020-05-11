adfbe38f57186b301d5bbaa52147cf05ffb1f71f
jnprogrammer <joshuanparker@gmail.com>
2020-05-10 22:40:04 -0400

    Renamed a file to be more inline with simular files, played with ranges in python

--- range3.py
+++ range3.py
@@ -1,7 +1,10 @@
+for i in range(10, 0, -2):
+    print("i is now {}".format(i))
+
 name = input("What's your name?  ")
 age = int(input("How old are you? "))
 
-if 18 <= age < 30:
+if age in range(16,66):
     print("You got on Vacation some times")
 elif age > 30:
     print("you should retire.")
