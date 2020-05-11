a9cc6e49309dac774393fd3488d721e2af8c55a7
jnprogrammer <joshuanparker@gmail.com>
2020-05-10 23:58:27 -0400

    Remade if game with whileloop

--- whileloopgame.py
+++ whileloopgame.py
@@ -3,5 +3,8 @@ available_exits = ["north", "south", "east", "west"]
 chosen_exit = ""
 while chosen_exit not in available_exits:
     chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() == "quit":
+        print("Game terminated")
+        break
 
 print("You got of there!!")
