bd28669776252311f58a92874087572aba0ce4be
joshuaparker <joshuanparker@gmail.com>
2020-05-13 22:22:37 -0400

    Exploring modules and the python library

---
+++ modules.py
@@ -0,0 +1,16 @@
+# print(dir())
+# # print(dir(__builtins__))
+# #
+# # for m in dir(__builtins__):
+# #     print(m)
+
+import shelve
+
+# print(dir())
+# print()
+# print(dir(shelve))
+#
+# for s in dir(shelve):
+#     print(s)
+
+help(shelve)
