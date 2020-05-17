e2d9186bcb7a064fea3cb93baa2653a630cd1162
jnprogrammer <joshuanparker@gmail.com>
2020-05-17 18:35:57 -0400

    Exploring nested functions

---
+++ functions4.py
@@ -0,0 +1,21 @@
+def spam1():
+
+    def spam2():
+
+        def spam3():
+            z = " even more spam"
+            print("in spam3, locals are {}".format(locals()))
+            return z
+
+        y = " more spam"
+        y += spam3()
+        print("In spam2, locals are {}".format(locals()))
+        return y
+
+    x = "spam"
+    x += spam2()
+    print("In spam1, locals are {}".format(locals()))
+    return x
+
+print(spam1())
+
