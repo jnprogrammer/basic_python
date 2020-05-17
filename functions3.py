0c1492e0ab03835a71e365c76eff3c431635b800
jnprogrammer <joshuanparker@gmail.com>
2020-05-17 03:03:00 -0400

    Added a factorial function

--- functions3.py
+++ functions3.py
@@ -2,6 +2,7 @@ def theCount(first, multi, divide):
     thecount = (first * multi) // divide
     print(thecount)
 
+
 def fact(n):
     """Calculate n iteratively"""
     results = 1
@@ -10,10 +11,15 @@ def fact(n):
             results *= f
     return results
 
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
 for i in range(130):
     print(i, fact(i))
 
-
-theCount(45,2,6)
-
-
+theCount(45, 2, 6)
