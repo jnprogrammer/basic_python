942c73cab997b25e86f6d88ed651f5af9b35c3a6
jnprogrammer <joshuanparker@gmail.com>
2020-05-17 21:42:47 -0400

    Learned how to prevent name Mangling by using double underscores on beginning and end of a member var.

--- objectmethods.py
+++ objectmethods.py
@@ -10,34 +10,35 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account made for " + self.name)
+        self._name = name
+        self.__balance__ = balance
+        self._transaction_list = []
+        self._transaction_list.append((Account._current_time(), self.__balance__))
+        print("Account made for " + self._name)
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance__ += amount
             self.show_bal()
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
+        if 0 < amount <= self.__balance__:
+            self.__balance__ -= amount
             self.show_bal()
-            self.transaction_list.append((Account._current_time(), -amount))
+            self._transaction_list.append((Account._current_time(), -amount))
 
         else:
             print("Insufficient funds.")
             self.show_bal()
 
     def show_bal(self):
-        print("Bal is {}".format(self.balance))
+        print("Bal is {}".format(self.__balance__))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
-                tran_type = "Depositied"
+                tran_type = "Deposited"
             else:
                 tran_type = "Withdrawn"
                 amount *= -1
@@ -46,7 +47,6 @@ class Account:
 
 if __name__ == '__main__':
     jim = Account("Jim", 0)
-    jim.show_bal()
 
     jim.deposit(1428)
     jim.withdraw(420)
@@ -54,3 +54,12 @@ if __name__ == '__main__':
     jim.deposit(344234)
     jim.withdraw(70400)
     jim.show_transactions()
+    print("$" * 100)
+    tank = Account("Tank", 7038443)
+    tank.deposit(8400)
+    tank.withdraw(200000)
+    tank.show_transactions()
+    tank.show_bal()
+    print(tank.__dict__)
+    tank._Account__balance = 40
+    tank.show_bal()
