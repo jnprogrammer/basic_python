e3dc7d10dedc1f2e0aad8b6fa8da64f2fbfc6e8e
joshuaparker <joshuanparker@gmail.com>
2020-05-18 23:06:45 -0400

    Built out interlinked classes, not always a good idea in python due to how garbage collection works but can be used in some cases.

--- objects3.py
+++ objects3.py
@@ -8,10 +8,10 @@ class Song:
     """
 
     def __init__(self, title, artist, duration=0):
-
         self.title = title
         self.artist = artist
         self.duration = duration
+
 
 class Album:
     """Class to represent Albam, using its track list
@@ -26,6 +26,7 @@ class Album:
     Methods:
         addSong: Used to add a new song to the album's track list
         """
+
     def __init__(self, name, year, artist=None):
         self.name = name
         self.year = year
@@ -50,3 +51,45 @@ class Album:
         else:
             self.tracks.insert(position, song)
 
+
+class Artist:
+    """Stores artists details
+
+    Attributes:
+        name (str): The name of the artist.
+        albums (List[Album]): A list of albums by this artist.
+        The list includes only those albums in this collection
+
+    Methods:
+        add_album: Use to add a new album to the artist's albums list
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        """
+        Add a new album to the list.
+
+        Args:
+            album (Album): Album object to add to the list.
+                If the album is already present, it will not add it again (future)
+        """
+        self.albums.append(album)
+
+
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+
+    with open("albums.txt", 'r') as albums:
+        for line in albums:
+            artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
+            year_field = int(year_field)
+            print(artist_field, album_field, year_field, song_field)
+
+
+if __name__ == '__main__':
+    load_data()
