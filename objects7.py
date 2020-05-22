a487a04b4a806ab66fbb2c4f7fe7bb79ecd3a8a1
joshuaparker <joshuanparker@gmail.com>
2020-05-21 23:39:14 -0400

    built more classes using composition to give classes more features

---
+++ objects7.py
@@ -0,0 +1,38 @@
+class Tag(object):
+
+    def __init__(self, name, contents):
+        self.start_tag = '<{}>'.format(name)
+        self.end_tag = '</{}>'.format(name)
+        self.contents = contents
+
+    def __str__(self):
+        return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
+
+    def display(self):
+        print(self)
+
+class Doc_Type(Tag):
+    def __init__(self):
+        super().__init('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
+        self.end_tag = '' #Doctype doesn't have an end tag
+
+class Head(Tag):
+
+    def __init__(self):
+        super().__init__('head', '')
+
+class Body(Tag):
+
+    def __init__(self):
+        super(Body, self).__init__('body', '')
+        self._body_contents = []
+
+    def add_tag(self, name, contents):
+        new_tag = Tag(name, contents)
+        self._body_contents.append(new_tag)
+
+    def display(self):
+        for tag in self._body_contents:
+            self.contents += str(tag)
+
+        super().display()
