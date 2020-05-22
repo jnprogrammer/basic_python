a5804c4514a811053d38ee3a7dfbe60d9d3e4865
joshuaparker <joshuanparker@gmail.com>
2020-05-21 23:23:32 -0400

    Using Composition in my study of polymorphism in python classes.

--- objects6.py
+++ objects6.py
@@ -1,4 +1,21 @@
+class Wing(object):
+
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("I'm Flying. This is fun")
+        elif self.ratio == 1:
+            print("This is heavy but I'm flying")
+        else:
+            print("I'm to heavy to fly")
+
+
 class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
 
     def walk(self):
         print("Waddle waddle waddle")
@@ -8,6 +25,9 @@ class Duck(object):
 
     def quack(self):
         print("Quack Quack Quack")
+
+    def fly(self):
+        self._wing.fly()
 
 
 class Penguin(object):
@@ -34,3 +54,4 @@ if __name__ == '__main__':
 
     pear = Penguin()
     test_duck(pear)
+    donald.fly()
