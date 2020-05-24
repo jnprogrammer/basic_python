3f28e290458d29dd4fa4746be5b61f419209a8e2
jnprogrammer <joshuanparker@gmail.com>
2020-05-23 23:55:23 -0400

    Updated database to contain more test data.

--- databases.py
+++ databases.py
@@ -3,8 +3,8 @@ import sqlite3
 db = sqlite3.connect("contacts.sqlite")
 
 db.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)")
-db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tom', 53790834, 'trythis@fakemail.com')")
-db.execute("INSERT INTO contacts VALUES('Blake', 42425345, 'blake@fakemail.com')")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('TEST DATA', 010101011, 'dont@fakemail.com')")
+db.execute("INSERT INTO contacts VALUES('Blankets', 5555555, 'bankers@fakemail.com')")
 
 cursor = db.cursor()
 cursor.execute("SELECT * FROM contacts")
