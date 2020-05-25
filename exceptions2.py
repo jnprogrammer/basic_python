bb247a3514742639ca535cf56fed2b51870fa047
jnprogrammer <joshuanparker@gmail.com>
2020-05-25 13:16:12 -0400

    Refactored try block to take multiple exceptions on one line

--- exceptions2.py
+++ exceptions2.py
@@ -2,13 +2,14 @@ def factorial(n):
     if n <= 1:
         return 1
     else:
+        print(n / 0)
         return n * factorial(n - 1)
 
 
 try:
     print(factorial(44243))
-except RecursionError:
-    print("Can't print factorials that big")
-except ZeroDivisionError:
+except (RecursionError, ZeroDivisionError):
+    print("Can't print factorials that big and")
     print("Can't divide by 0, you know this!")
 print("Program ending")
+
