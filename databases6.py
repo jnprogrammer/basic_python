50f2f20695b2b6a5fe0ec34e21148b23e18bbf9f
joshuaparker <joshuanparker@gmail.com>
2020-05-26 19:00:50 -0400

    Build a GUI in python using tkinter to explore the DB

---
+++ databases6.py
@@ -0,0 +1,53 @@
+import tkinter, sqlite3
+
+con = sqlite3.connect('music.sqlite')
+
+mainWindow = tkinter.TK()
+mainWindow.title('Music DB Browser')
+mainWindow.geometry('1024x768')
+
+mainWindow.columnconfigure(0, weight=2)
+mainWindow.columnconfigure(1, weight=2)
+mainWindow.columnconfigure(2, weight=2)
+mainWindow.columnconfigure(3, weight=2) #spacer column
+
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=5)
+mainWindow.rowconfigure(2, weight=5)
+mainWindow.rowconfigure(3, weight=1)
+
+# ==== labels
+
+tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
+tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
+tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
+
+# +++ Artists
+
+artistList = tkinter.Listbox(mainWindow)
+artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
+artistList.config(boarder=2, relief='sunken')
+
+# TODO: Albums listbox
+
+albumLV = tkinter.Variable(mainWindow)
+albumLV.set(("Choose an artist",))
+albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
+albumList.grid(row=1, column=1, sticky='nsew', padx=(30,0))
+albumList.config(boarder=2, relief='sunken')
+
+# Songs list box
+
+songLV = tkinter.Variable(mainWindow)
+songLV.set(("Choose an album", ))
+songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
+songList.config(boarder=2, relief='sunken')
+
+
+# main loop
+
+mainWindow.mainloop()
+print("Closing database connection")
+con.close()
+
