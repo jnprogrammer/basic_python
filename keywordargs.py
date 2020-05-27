8f5015558e4249a4f15294f152968c31a1775d8c
joshuaparker <joshuanparker@gmail.com>
2020-05-26 21:42:57 -0400

    I've learned that kwargs are great for changing the behavior of functions.

--- keywordargs.py
+++ keywordargs.py
@@ -2,9 +2,14 @@
 
 def print_bw(*args, **kwargs):
     print(kwargs)
+    kwargs.pop('end', None)
     for word in args[::-1]:
         print(word[::-1], end=' ', **kwargs)
 
 words =("Hey", "IT's ", "TIME", "FOR", "MORTAL", "KOMBAT")
 with open("backwards.txt", 'w') as backwords:
-    print_bw("Hey", "IT's ", "TIME", "FOR", "MORTAL", "KOMBAT",file=backwords)
+    print_bw("Hey", "IT's ", "TIME", "FOR", "MORTAL", "KOMBAT") #end=' '
+
+# Can't set multiple values for the same keyword argument with **kwargs
+# def print_bw(*args, end=' ', **kwargs):
+
