4ad80b738d1a5ea5c32989cb96031996b1d85b08
joshuaparker <joshuanparker@gmail.com>
2020-05-28 21:52:32 -0400

    Updated folder crawler.

--- generators3.py
+++ generators3.py
@@ -2,12 +2,24 @@ import os
 
 if __name__ == '__main__':
 
-    root = "/music"  # type: str
+    root = "music"  # type: str
 
     for path, directory, files, in os.walk(root, topdown=True):
-        print(path)
-        print(directory)
-        print(files)
-        input()
+       if files:
+           print(path)
+           first_split = os.path.split(path)
+           print(first_split)
+           second_split = os.path.split(path)
+           print(second_split)
+           for f in files:
+               song_details = f[:-5].split(' _ ')
+               print(song_details)
+           print("@" * 50)
+
+
+        # print(path)
+        # print(directory)
+        # print(files)
+        # input()
         # for f in files:
         #     print("\t{}".format(f))
