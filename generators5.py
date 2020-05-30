5e3ee67299b63ba0d3572f2a256dfeeea0e0de4a
jnprogrammer <joshuanparker@gmail.com>
2020-05-29 23:57:36 -0400

    Completed music generators. . .honestly i can use a forloop

--- generators5.py
+++ generators5.py
@@ -3,8 +3,10 @@ import os, fnmatch
 def find_music(start, extension):
     for path, directories, files, in os.walk(start):
         for file in fnmatch.filter(files, '*.{}'.format(extension)):
-            yield os.path.join(path, file)
+            absolute_path = os.path.abspath(path)
+            yield os.path.join(absolute_path, file)
 
 
+my_music_files = find_music('music', 'emp3')
 for f in find_music('music', 'emp3'):
     print(f)
