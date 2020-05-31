9ce6bdaca2eada69b48b13b9b432b974f7c7f711
joshuaparker <joshuanparker@gmail.com>
2020-05-30 23:49:09 -0400

    Added a downloaded python class for reading mp3 data and incorporated into my code for extracting mp3 data.

--- generators5.py
+++ generators5.py
@@ -1,4 +1,6 @@
 import os, fnmatch
+import id3reader_p3 as id3reader
+
 
 def find_music(start, extension):
     for path, directories, files, in os.walk(start):
@@ -8,5 +10,23 @@ def find_music(start, extension):
 
 
 my_music_files = find_music('music', 'emp3')
+
+error_list = []
+
+
 for f in find_music('music', 'emp3'):
-    print(f)
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
+            id3r.get_value('performer'),
+            id3r.get_value('album'),
+            id3r.get_value('track'),
+            id3r.get_value('title')
+        ))
+    except:
+        error_list.append(f)
+
+
+print("generating improper mp3 files")
+for error_file in error_list:
+    print(error_file)
