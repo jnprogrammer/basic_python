f55b5fb1c0785afe6739c4a31642240bfb4cd200
joshuaparker <joshuanparker@gmail.com>
2020-06-01 18:49:31 -0400

    made a 10 times table using list comps

--- listcomprehension4.py
+++ listcomprehension4.py
@@ -1,16 +1,22 @@
 #nested comprehensions
 
-burgers = ["regular", "Veggy", "Spicy"]
+burgers = ["regular", "Veggy", "wHO DID THIS !?!?"]
 toppings = ["Cheese\nTomatos\nMustard\nKetchup", "Pepper Jack Cheese\nTomatos\nHot Buffalo sauce\nSpicy Mustard", "cats", "Other . . ."]
 
 meals = [(buger, toppings) for buger in burgers for topping in toppings]
-print(meals)
+# print(meals)
 
 print()
 
 
-for burger in burgers:
-    for topping in toppings:
-        print((burger, topping))
+# for burger in burgers:
+#     for topping in toppings:
+#         print((burger, topping))
 
+
+for meals in [(buger, toppings) for buger in burgers for topping in toppings]:
+    print(meals)
+
+for meals in [(burger, toppings) for topping in toppings for burger in burgers]:
+    print(meals)
 
