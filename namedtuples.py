94268179248e132a3c0b381f0f7db419fe94d585
joshuaparker <joshuanparker@gmail.com>
2020-06-01 22:10:33 -0400

    Did some work with namedtuples

---
+++ namedtuples.py
@@ -0,0 +1,16 @@
+from namedtuples_a import basic_plants_list, plants_list
+
+
+print(plants_list[0])
+
+
+for plant in plants_list:
+    print(plant.name, plant[1])
+
+print()
+
+
+example = plants_list[0]
+print(example)
+example = example._replace(lifecycle='Annual')
+print(example)
